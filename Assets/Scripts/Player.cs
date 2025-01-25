1680127a92c5935168f5c75870e343fb061dd078
DarkTiger <darktigerdevelop@gmail.com>
2025-01-25 14:59:47 +0100

    Implemented Dash mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,15 +7,29 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
     [SerializeField] float maxSpeed = 5f;
-    Rigidbody rb = null;
+    [SerializeField] float maxSpeedDash = 10f;
+    [SerializeField] float dashForce = 5f;
+    [SerializeField] float dashTime = 0.25f;
+    [SerializeField] float dashCooldown = 5f;
+    float baseMaxSpeed = 1f;
+    float currentDashTime = 0f;
+    float currentDashCooldown = 0f;
+
     PlayerInput playerInput = null;
     InputAction moveAction = null;
+    InputAction jumpAction = null;
+    Rigidbody rb = null;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+
+        baseMaxSpeed = maxSpeed;
+        currentDashCooldown = 0f;
     }
 
     private void Update()
@@ -25,6 +40,36 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(moveValue.x * speed, moveValue.y * speed, 0f);
             rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxSpeed);
-        }       
+        }
+
+        currentDashCooldown -= Time.deltaTime;
+
+        Dash();
+    }
+
+    void Dash()
+    {
+        if (currentDashCooldown > 0f) return;
+
+        if (jumpAction.WasPerformedThisFrame())
+        {
+            StartCoroutine(DashCO());
+        }
+    }
+
+    IEnumerator DashCO()
+    {
+        currentDashCooldown = dashCooldown;
+
+        while (currentDashTime < dashTime)
+        {
+            rb.AddForce(rb.linearVelocity.normalized * dashForce, ForceMode.Force);
+            currentDashTime += Time.deltaTime;
+            maxSpeed = maxSpeedDash;
+            yield return null;
+        }
+
+        maxSpeed = baseMaxSpeed;
+        currentDashTime = 0f;
     }
 }
