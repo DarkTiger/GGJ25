3b403f6378772d65f8dfd71427c75900b320abf3
DarkTiger <darktigerdevelop@gmail.com>
2025-01-26 00:26:39 +0100

    Implemented players bobble attachment parts

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    public ShapeType ShapeIndex = ShapeType.Sphere;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,14 @@ public class Player : MonoBehaviour
     [SerializeField] float dashForce = 5f;
     [SerializeField] float dashTime = 0.25f;
     [SerializeField] float dashCooldown = 5f;
+    [SerializeField] Transform shapesParent = null; 
 
     public ShapeType CurrentShapeIndex { get; private set; } = 0;
     public int CurrentSpheresCount { get; set; } = 0;
     public int CurrentCubesCount { get; set; } = 0;
     public int CurrentPiramidsCount { get; set; } = 0;
+
+    public static Player Instance = null; 
 
     float baseMaxSpeed = 1f;
     float currentDashTime = 0f;
@@ -29,6 +32,7 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
+        Instance = this;
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
@@ -88,11 +92,53 @@ public class Player : MonoBehaviour
 
     public void ChangeShape(ShapeType shapeIndex)
     {
-        for (int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < 3; i++)
         {
             transform.GetChild(i).gameObject.SetActive((int)shapeIndex == i);
             CurrentShapeIndex = shapeIndex;
         }
+    }
+
+    public void ResetStats()
+    {
+        CurrentSpheresCount = 0;
+        CurrentCubesCount = 0;
+        CurrentPiramidsCount = 0;
+
+        ResetBodyShapes();
+    }
+
+    public void NewBodyShape()
+    {
+        ResetBodyShapes();
+
+        StartCoroutine(NewBodyShapeCO());
+    }
+
+    IEnumerator NewBodyShapeCO()
+    {
+        yield return null;
+        shapesParent.GetChild(GameManager.Instance.CurrentShapeIndex).gameObject.SetActive(true);
+    }
+
+    public void ResetBodyShapes()
+    {
+        StartCoroutine(ResetBodyShapesCO());
+    }
+
+    IEnumerator ResetBodyShapesCO()
+    {
+        for (int i = 0; i < shapesParent.childCount; i++)
+        {
+            for (int j = 0; j < shapesParent.GetChild(i).childCount; j++)
+            {
+                shapesParent.GetChild(i).GetChild(j).gameObject.SetActive(false);
+            }
+
+            shapesParent.GetChild(i).gameObject.SetActive(false);
+        }
+
+        yield return null;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -114,27 +160,59 @@ public class Player : MonoBehaviour
 
         if (other.CompareTag("Bubble"))
         {
-            if (CurrentShapeIndex != GameManager.Instance.CurrentShapeData.PlayerShapeType)
-            {GameManager.Instance.Error();
-                
+            ShapeType bubbleType = other.GetComponent<Bubble>().ShapeIndex;
+
+            if (CurrentShapeIndex != GameManager.Instance.CurrentShapeData.PlayerShapeType || 
+                (bubbleType == ShapeType.Sphere && CurrentSpheresCount + 1 > GameManager.Instance.CurrentShapeData.SpheresCount ||
+                bubbleType == ShapeType.Cube && CurrentCubesCount + 1 > GameManager.Instance.CurrentShapeData.CubesCount ||
+                bubbleType == ShapeType.Piramid && CurrentPiramidsCount + 1 > GameManager.Instance.CurrentShapeData.PiramidsCount))
+            {
+                GameManager.Instance.Error();
             }
             else
             {
+                Transform bodyParts = shapesParent.GetChild(GameManager.Instance.CurrentShapeIndex);
+
                 switch (other.GetComponent<Bubble>().ShapeIndex)
                 {
                     case ShapeType.Sphere:
                         CurrentSpheresCount++;
+                        for (int i = 0; i < bodyParts.childCount; i++)
+                        {
+                            if (bodyParts.GetChild(i).GetComponent<BodyPart>().ShapeIndex == ShapeType.Sphere && !bodyParts.GetChild(i).gameObject.activeSelf)
+                            {
+                                bodyParts.GetChild(i).gameObject.SetActive(true);
+                                break;
+                            }
+                        }
                         break;
                     case ShapeType.Cube:
                         CurrentCubesCount++;
+                        for (int i = 0; i < bodyParts.childCount; i++)
+                        {
+                            if (bodyParts.GetChild(i).GetComponent<BodyPart>().ShapeIndex == ShapeType.Cube && !bodyParts.GetChild(i).gameObject.activeSelf)
+                            {
+                                bodyParts.GetChild(i).gameObject.SetActive(true);
+                                break;
+                            }
+                        }
                         break;
                     case ShapeType.Piramid:
                         CurrentPiramidsCount++;
+                        for (int i = 0; i < bodyParts.childCount; i++)
+                        {
+                            if (bodyParts.GetChild(i).GetComponent<BodyPart>().ShapeIndex == ShapeType.Piramid && !bodyParts.GetChild(i).gameObject.activeSelf)
+                            {
+                                bodyParts.GetChild(i).gameObject.SetActive(true);
+                                break;
+                            }
+                        }
                         break;
                 }
+
+                GameManager.Instance.OnPlayerChanged(CurrentShapeIndex, CurrentSpheresCount, CurrentCubesCount, CurrentPiramidsCount);
             }
 
-            GameManager.Instance.OnPlayerChanged(CurrentShapeIndex, CurrentSpheresCount, CurrentCubesCount, CurrentPiramidsCount);
             Destroy(other.gameObject);
         }
     }
