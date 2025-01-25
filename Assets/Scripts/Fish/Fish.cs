f1ba40f00ae23937c7b94e023b608064e6c80fe3
Attilio Oliva <attilio.oliva@protonmail.com>
2025-01-25 21:32:28 +0100

    feat: add head rotation to fishes

--- Assets/Scripts/Fish/Fish.cs
+++ Assets/Scripts/Fish/Fish.cs
@@ -14,9 +14,12 @@ public class Fish : MonoBehaviour
     public float PatternIntensity = 5.0f;
 
     public float PatternDurationTime = 1.0f;
+    public bool ShouldRotateAlongPath = true;
+    public float RotationIntensity = 1.0f;
 
     private float currentLifetime = 0f;
     private Vector3 initialPosition = Vector3.zero;
+
 
     protected virtual void Move() {
         float patternTime = currentLifetime % PatternDurationTime;
@@ -30,6 +33,16 @@ public class Fish : MonoBehaviour
         transform.position += new Vector3(RightMovementIntensity * Speed * Time.deltaTime, 0f, 0f);
     }
 
+/// <summary>
+/// Rotates the fish face to the direction it is moving exagerating the rotation according to a RotationIntensity
+/// </summary>
+    void RotateAlongPath()
+    {
+        Vector3 direction = (transform.position - initialPosition).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle * RotationIntensity);
+    }
+
     void Start()
     {
         initialPosition = transform.position;
@@ -40,6 +53,9 @@ public class Fish : MonoBehaviour
     protected virtual void Update()
     {
         Move();
+        if (ShouldRotateAlongPath){
+            RotateAlongPath();
+        }
         currentLifetime += Time.deltaTime;
     }
 }
