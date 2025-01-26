1b7f50b27071ff94b4e76f0b1f8891cc8b6f3d0c
DarkTiger <darktigerdevelop@gmail.com>
2025-01-26 17:12:00 +0100

    Improved main menu with intro

--- Assets/Scripts/UI/mainManu.cs
+++ Assets/Scripts/UI/mainManu.cs
@@ -1,14 +1,36 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class mainManu : MonoBehaviour
 {
+    [SerializeField] GameObject Intro;
+    [SerializeField] GameObject MainMenu;
     [SerializeField] GameObject Tutorial;
     [SerializeField] GameObject Credits;
 
-   public void GO()
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(5);
+
+        Intro.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+    public void GO()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void IntroEnable(bool enable)
+    {
+        Intro.SetActive(enable);
+    }
+
+    public void MainMenuEnable(bool enable)
+    {
+        MainMenu.SetActive(enable);
     }
 
     public void TutorialEnable(bool enable)
