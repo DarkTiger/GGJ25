9946c760d8c17a5995be41bdf65d6eac772460e9
DarkTiger <darktigerdevelop@gmail.com>
2025-01-26 16:35:33 +0100

    Implemented tutorial, credits and music

--- Assets/Scripts/UI/mainManu.cs
+++ Assets/Scripts/UI/mainManu.cs
@@ -3,11 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class mainManu : MonoBehaviour
 {
+    [SerializeField] GameObject Tutorial;
+    [SerializeField] GameObject Credits;
+
    public void GO()
     {
         SceneManager.LoadSceneAsync(1);
-
     }
+
+    public void TutorialEnable(bool enable)
+    {
+        Tutorial.SetActive(enable);
+    }
+
+    public void CreditsEnable(bool enable)
+    {
+        Credits.SetActive(enable);
+    }
+
     public void quitgame()
     {
         Application.Quit();
